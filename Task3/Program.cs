3389aee15c544dac415b89d5e08481c628bf6eb3
Behruz <behruzk2002@mail.ru>
2024-11-06 00:27:00 -0500

    Added and completed Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив из вещественных чисел с
+// ненулевой дробной частью. Найдите разницу между
+// максимальным и минимальным элементов массива.
+
+double[] array = new double[5] { -2, 5, 7.6, 11.6, 55.1 };
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (max > array[i])
+    {
+        max = array[i];
+    }
+    if (min < array[i])
+    {
+        min = array[i];
+    }
+}
+double diff = max - min;
+Console.WriteLine(diff);
